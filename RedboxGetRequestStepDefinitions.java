c4d7e3ddb5ef6ec42e7f342443c211a1741e1a9c
Mattcamalan <86687558+Mattcamalan@users.noreply.github.com>
2021-06-29 21:55:00 -0500

    added redboxtask

---
+++ RedboxGetRequestStepDefinitions.java
@@ -0,0 +1,53 @@
+package ui_test.ui_stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import utils.Driver;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import static io.restassured.RestAssured.given;
+
+public class RedboxGetRequestStepDefinitions {
+
+    Response response;
+    List<Map<String, Object>> deserializedList;
+    WebDriver driver= Driver.driver();
+
+    @When("User runs get request")
+    public void user_runs_get_request() {
+        response = given().accept(ContentType.JSON)
+                .when().get("https://preprod.redbox.com/rbweb/api/product/js/__titles7")
+                .then().extract().response();
+    }
+
+    @Then("Navigate to movie detail page and validate title name")
+    public void Navigate_to_movie_detail_page_and_validate_title_name() throws InterruptedException {
+
+        // convert json to java
+        deserializedList = response.as(new TypeRef<List<Map<String, Object>>>() {});
+        //System.out.println(deserializedList.size());
+
+        //Generate random number between given two values
+        Random r = new Random();
+        int low = 0;
+        int high = deserializedList.size();
+        int result = r.nextInt(high-low) + low;
+
+        Map<String,Object> mapMovie = deserializedList.get(result);
+        //System.out.println(mapMovie.get("url"));
+        driver.get("https://www.redbox.com/"+mapMovie.get("url"));
+        Thread.sleep(1000);//in order to load the element page title i need to put sleep thread here
+        String pageTitle = driver.getTitle();
+        String movieName = mapMovie.get("name").toString();
+
+        Assert.assertTrue(pageTitle.contains(movieName));
+    }
+}
---
+++ RedboxTasks.feature
@@ -0,0 +1,23 @@
+Feature: Test RedBox APIs
+
+  @task1
+  Scenario: Get all movies
+    When User runs get request
+    Then Navigate to movie detail page and validate title name
+
+  @task2
+  Scenario: Movie search
+    When Navigate to search page
+    Then Search movie name and validate title name on movie detail page
+
+  @task3
+  Scenario: Search comparision
+    When Go to search page
+    Then Search movie name and validate search text on search result page
+
+  @task3-2
+  Scenario: Auto complete comparision
+    When Go to search page
+    Then Compare auto complete result vs results on the search results page
+
+
